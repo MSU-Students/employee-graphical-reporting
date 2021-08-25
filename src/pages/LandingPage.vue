7c76663697c8e1d2d5d376a9cd02b4543d6a47eb
rshylmrhm <reshylmaruhom@gmail.com>
2021-08-25 11:09:46 -0700

    initial commit

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page>
+
+    <div class="col">
+      <q-card>
+        <img
+          class="col text-center q-pt-md"
+          src="~assets/images/HRDO.jpg"
+          style="height: 500px"
+        />
+      </q-card>
+    </div>
+    </q-page>
+
+    <div class="text-center text-bold text-h3 text-italic q-pt-xxs-xxs">
+      MSU HRDO Management Information System with Graphical Reporting
+    </div>
+
+    <div class="col q-pt-xl">
+      <q-card class="bg-grey text-center" flat>
+        <q-card-section>
+          {{ message }}
+        </q-card-section>
+      </q-card>
+    </div>
+
+    <div class="q-pa-md q-gutter-sm">
+    <q-btn color="grey-5" text-color="black" label="login" to="LoginPage"/>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message:
+        "Is a system that focuses on analyzing employee’s information using graphical presentation that may possibly be useful to come up with decision making to develop strategies for the future situations. It is intended to help and to provide a management information system attached with graphical reporting for the Human Resource Development Office in Mindanao State University Main Campus, Marawi City with the help of an automated graphical reporting that can handle and measure large amount of data to provide any graphical presentation.",
+    };
+  },
+};
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,62 @@
+<template>
+
+<div class="absolute-center ">
+  <q-card class="bg-grey text-center">
+     <div class= "text-center text-h5 text-bold text-italic">
+    Login Page
+  </div>
+
+ <div>
+   
+    <div class="q-pa-md">
+      <div class="q-gutter-md row items-start">
+      <q-input v-model= "name" label="Username" style="width:300px" />
+      </div>
+    </div>
+
+      <q-input v-model= "pass" label="Password" filled :type="isPwd ? 'Password' : 'text'">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+  </div>
+
+    <div class="q-pa-md" color="black">
+    <div class="q-gutter-sm">
+      <q-radio v-model="shape" val="Admin" label="Admin" />
+      <q-radio v-model="shape" val="Director" label="Director" />
+    </div>
+    <div class="q-px-sm">
+      Your selection is: <strong>{{ shape }}</strong>
+    </div>
+  </div>
+
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn color="grey-5" text-color="black" label="login" style="width:250px"/>
+  </div>
+  </q-card>
+  
+<div class="q-pa-md q-gutter-sm">
+    <q-btn color="grey-5" text-color="black" label="back" to= "LandingPage"/>
+</div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      pass: '',
+      isPwd: true,
+      shape: ''
+    }
+  }
+}
+</script>
