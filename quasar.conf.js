df26ac7cc3af3ae04b6ea15dffa92807f319aa04
usman0512 <usman12strygwyr@gmail.com>
2021-10-16 14:03:44 +0800

    change tab name from Quasar App into Graphical Report

--- quasar.conf.js
+++ quasar.conf.js
@@ -133,8 +133,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Graphical Report`,
+        short_name: `Graphical Report`,
         description: `A Quasar Framework app`,
         display: 'standalone',
         orientation: 'portrait',
